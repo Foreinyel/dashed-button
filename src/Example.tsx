4f039603dee7dab99dc8efec2f23a2ea4c3e8375
Foreinyel <Foreinyel@163.com>
2022-06-12 22:34:06 +0800

    example

--- src/Example.tsx
+++ src/Example.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import Component from "./Component";
 
 export const Example = () => {
-  return <Component />;
+  return (
+    <div>
+      <Component
+        onClick={() => {
+          alert("dashed button clicked");
+        }}
+      >
+        Dashed Button
+      </Component>
+    </div>
+  );
 };
 
 export default Example;
